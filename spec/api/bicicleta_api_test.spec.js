4bd384dfa55f75dddab93e6741e59de5854c469e
RaulZury <mrrz272a@gmail.com>
2021-05-04 00:50:06 -0500

    Se añade controller usuario, spec y varias modificaciones

--- spec/api/bicicleta_api_test.spec.js
+++ spec/api/bicicleta_api_test.spec.js
@@ -1,32 +1,58 @@
+var mongoose = require('mongoose');
 var Bicicleta = require('../../models/bicicleta');
 var request = require('request');
 var server = require('../../bin/www');
-const { response } = require('../../app');
+//const { response } = require('../../app');
+
+var base_url = "http://localhost:3000/api/bicicletas";
 
 describe('Bicicleta API', ()=>{
-    describe('GET Bicicletas', () =>{
-        it('Status 200', ()=>{
+    beforeEach(function(done){
+        var mongoDB = 'mongodb://localhost/testdb';
+        mongoose.conect(mongoDB, {useNewUrlParser: true});
+
+        const db = mongoose.connection;
+        db.on('error', console.error.bind(console, 'connection error'));
+        db.once('open', function(){
+            console.log('We are connected to test database');
+            done();
+        });
+    });
+    afterEach(function(done){
+        Bicicleta.deleteMany({}, function(err, success){
+            if(err) console.log(err);
+            done();
+        });
+    });
+
+    describe("GET Bicicletas /", () =>{
+        it("Status 200", (done) =>{
             expect(Bicicleta.allBicis.length).toBe(0);
 
-            var a = new Bicicleta(1, 'blanco', 'MTB', [19.47520, -99.04549]);
-            Bicicleta.add(a);
-
-            request.get('http://localhost:3000/api/bicicletas', function(error, response, body){
-                expect(response.statusCode).toBe(500);
+            request.get(base_url, function(error, response, body){
+                var result = JSON.parse(body);
+                expect(reponse.statusCode).toBe(200);
+                expect(result.bicicletas.length).toBe(0);
+                done();
             });
         });
     });
+
     describe('POST Bicicletas /create', ()=>{
         it('Status 200', (done)=>{
             var headers = {'content-type': 'aplication/json'};
-            var aBici = '{"id": 1, "color": "blanco", "modelo": "MTB", "lat": 19, "lng": -99}';
+            var aBici = '{"code": 10, "color": "blanco", "modelo": "MTB", "lat": 19, "lng": -99}';
             request.post({
                 headers: headers,
-                url: 'http://localhost:3000/api/bicicletas/create',
+                url: base_url + '/create',
                 body: aBici
             }, function(error, response, body){
                 expect(response.statusCode).toBe(200);
-                expect(Bicicleta.findById(1).color).toBe("blanco");
+                var bici = JSON.parse(body).bicicleta;
+                console.log(bici);
+                expect(bici.color).toBe("blanco");
+                expect(bici.ubicacion[0]).toBe(19);
+                expect(bici.ubicacion[1]).toBe(-99);
                 done();
             });
         });
--- spec/models/bicicleta_test.spec.js
+++ spec/models/bicicleta_test.spec.js
@@ -1,6 +1,80 @@
+var mongoose = require('mongoose');
 var Bicicleta = require('../../models/bicicleta');
 
-beforeEach(() => {Bicicleta.allBicis= [];});
+describe('testing bicicletas', function(){
+    beforeAll((done)=>{
+        var mongoDB = 'mongodb://localhost/testdb';
+        mongoose.connect(mongoDB,{useNewUrlParser:true});
+        const db = mongoose.connection;
+        db.on('error',console.error.bind(console,'connection error'));
+        db.once('open',()=>{
+            console.log('We are connected to test database!');
+            done();
+        })
+    });
+    afterEach((done)=>{
+        Bicicleta.deleteMany({},(err,success)=>{
+            if(err) console.log(err);
+            done();
+        })
+    });
+    describe('Bicicleta.createInstance',()=>{
+        it('crea una instancia de Bicicleta',()=>{
+            var bici = Bicicleta.createInstance(1,"verde","urbana",[19,-99]);
+            expect(bici.code).toBe(1)
+            expect(bici.color).toBe("verde")
+            expect(bici.modelo).toBe("urbana")
+            expect(bici.ubicacion[0]).toBe(19)
+            expect(bici.ubicacion[1]).toBe(-99)
+        })
+    })
+    describe('Bicicleta.allBicis',()=>{
+        it('comienza vacia',(done)=>{
+            Bicicleta.allBicis(function(err,bicis){
+                expect(bicis.length).toBe(0);
+                done();
+            });
+        });
+    });
+    describe('Bicicleta.add',()=>{
+        it('agrega una bici',(done)=>{
+            var bici = new Bicicleta({code:1,color:"verde",modelo:"urbana"})
+            Bicicleta.add(bici,(err,newBici)=>{
+                if(err) console.log(err);
+                Bicicleta.allBicis((err,bicis)=>{
+                    expect(bicis.length).toEqual(1);
+                    expect(bicis[0].code).toEqual(bici.code);
+                    done();
+                });
+            });
+        });
+    });
+    describe('Bicicleta.findByCode', ()=>{
+        it('Debe devolver bici con code 1', (done)=>{
+            Bicicleta.allBicis(function(err, bicis){
+                expect(bicis.length).toBe(0);
+
+                var aBici = new Bicicleta({code: 1, color: "amarilla", modelo: "MTB"});
+                Bicicleta.add(aBici, function(err, newBici){
+                    if(err) console.log(err);
+                
+                    var aBici2 = new Bicicleta({code: 2, color: "rojo", modelo: "carrera"});
+                    Bicicleta.add(aBici2, function(err, newBici){
+                        if(err) console.log(err);
+                        Bicicleta.findByCode(1, function(err, targetBici){
+                            expect(targetBici.code).toBe(aBici.code);
+                            expect(targetBici.color).toBe(aBici.color);
+                            expect(targetBici.modelo).toBe(aBici.modelo);
+                            done();
+                        });
+                    });
+                });
+            });
+        });
+    });
+});
+
+/*beforeEach(() => {Bicicleta.allBicis= [];});
 
 describe('Bicicleta.allBicis', ()=>{
     it('comienza vacía', () => {
@@ -42,4 +116,4 @@ describe('Bicicleta.removeById',()=>{
     Bicicleta.add(aBici2);
     Bicicleta.removeById(1);
     });
-});
+});*/
